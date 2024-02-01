38b9907ac55bd2763985c54dc65639129f45fce6
UptownNate <nathanhogg@gmail.com>
2024-02-01 09:32:30 -0600

    Add Greetings
    Add greetings

--- mixedmessages.js
+++ mixedmessages.js
@@ -1,1 +1,38 @@
-console.log("Hello");
+//Object to hold the parts of the greeting
+const greetingVault = () => {
+    const store = {
+        intro: [
+            'Howdy neighbour!',
+            'Top of the morning to ya!',
+            'How areyou doing my friend?',
+            'Look what the cat dragged in!',
+            'Looks like a beautiful day!'
+        ],
+        segway: [
+            'You know what they say.',
+            'My grandpa always said',
+            'As the saying goes,',
+            'A wise man once said ',
+            'My mom told me '
+        ],
+        proverbs: [
+            'Absence makes the heart grow fonder.',
+            'An apple a day keeps the doctor away.',
+            'The squeaky wheel gets the grease',
+            'The grass is always greener on the other side',
+            'The pen is mightier than the sword',
+            'All good things must come to an end',
+            'Don\'t bite the hand that feeds you',
+            ''    
+        ]
+    }
+}
+//Array to hold all the parts of the greeting
+const finalGreeting = [];
+
+//choose one from each of the 3 options (using Math.random and Math.floor)
+
+
+//test - console.log('Hello World');
+
+//present greeting
